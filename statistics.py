f8987ea97ef7424eff2cece8b0140621207be0a3
Pasu Sangiemsin <opxz7148@gmail.com>
2024-09-09 14:26:09 +0700

    improve docstring

--- statistics.py
+++ statistics.py
@@ -1,3 +1,5 @@
+"""Module contain statistical calculation function."""
+
 from math import sqrt
 
 
@@ -9,7 +11,7 @@ def average(data):
 
 
 def variance(data):
-    """the population variance of a list of numbers in data.
+    """Return population variance of a list of numbers in data.
 
     The variance is the sum of squared differences between data values
     and their mean, divided by the number of items in the list.
@@ -41,5 +43,5 @@ def variance(data):
 
 
 def stdev(data):
-    """the standard deviation of a list of values"""
+    """Return standard deviation of a list of values."""
     return sqrt(variance(data))
