656cbf99c8c5ed3317f89e95d974a1b82be95a86
Pasu Sangiemsin <opxz7148@gmail.com>
2024-09-16 15:38:07 +0700

    Add more then for more coverage

--- test_statistics.py
+++ test_statistics.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from statistics import variance, stdev
+from statistics import variance, stdev, average
 from math import sqrt
 
 
@@ -17,7 +17,12 @@ class StatisticsTest(TestCase):
         self.assertAlmostEqual(4.0, variance([0.1, 4.1]))
         # variance([0,4,4,8]) == 8
         self.assertAlmostEqual(8.0, variance([0.1, 4.1, 4.1, 8.1]))
-
+        
+    def test_variance_no_value(self):
+        """variance should raise an error when list is empty"""
+        with self.assertRaises(ValueError):
+            variance([])
+        
     def test_stdev(self):
         # standard deviation of a single value should be zero
         self.assertEqual(0.0, stdev([10.0]))
@@ -25,6 +30,18 @@ class StatisticsTest(TestCase):
         self.assertEqual(2.0, stdev([1, 5]))
         # variance([0, 0.5, 1, 1.5, 2.0]) is 0.5
         self.assertEqual(sqrt(0.5), stdev([0, 0.5, 1, 1.5, 2]))
+        
+    def test_average(self):
+        
+        data = [5, 5, 5, 5, 5, 5]
+        self.assertEqual(average(data), 5)
+        
+        data = [10, 10, 10, 5, 5, 5]
+        self.assertEqual(average(data), sum(data) / len(data))
+        
+        data = []
+        with self.assertRaises(ValueError):
+            average(data)
 
 
 if __name__ == '__main__':
